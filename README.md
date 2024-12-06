8fd2a2e516ede868ed19fa5935f2de8c409e78de
Alex Bozhenko <alexbozhenko@users.noreply.github.com>
2024-12-06 03:18:52 -0800

    Upd nats.ws to new nats.js library (#2)

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # NATS Whiteboard
 
-This project is a simple (~100 lines of JavaScript) implementation of a real-time, persistent interactive whiteboard using [NATS Websockets](https://github.com/nats-io/nats.ws), [NATS Jetstream](https://docs.nats.io/nats-concepts/jetstream) and AlpineJS.
+This project is a simple (~100 lines of JavaScript) implementation of a real-time, persistent interactive whiteboard using [nats.js](https://github.com/nats-io/nats.js), [NATS Jetstream](https://docs.nats.io/nats-concepts/jetstream) and AlpineJS.
 
 ## Demo
 To see the NATS whiteboard in action, feel free to [try the demo](https://nats-whiteboard.onrender.com/)
--- main.js
+++ main.js
@@ -1,37 +1,37 @@
 import './style.css'
 import Alpine from "alpinejs";
 import throttle from './throttle';
-import { connect, consumerOpts, headers, JSONCodec } from 'nats.ws';
+import { wsconnect, headers } from '@nats-io/nats-core';
+import { jetstream } from "@nats-io/jetstream";
 
 Alpine.data("whiteboard", (subject) => ({
   id: Math.random().toString(36).slice(2, 10),
   color: "black",
   thickness: 5,
   drawing: false,
-  last: { x:0, y:0 },
+  last: { x: 0, y: 0 },
   context: null,
   nats: null,
-  jc: null,
 
   async init() {
-    const server = import.meta.env.VITE_NATS_SERVER
-    this.jc = JSONCodec()
-    this.nats = await connect({ servers: server })
+    const server = "ws://" + window.location.hostname + ":9222"
+    this.nats = await wsconnect({ servers: server })
 
-    const opts = consumerOpts()
-    opts.orderedConsumer()
-    const sub = await this.nats.jetstream().subscribe(subject, opts)
+    const stream = "whiteboard";
+    const js = jetstream(this.nats);
+    const orderedConsumer = await js.consumers.get(stream);
+    const sub = await orderedConsumer.consume();
 
-    for await(const m of sub) {
-      const data = this.jc.decode(m.data)
+    for await (const m of sub) {
+      const data = m.json()
       switch (data.type) {
         case "draw":
-          if(data.id !== this.id) {
+          if (data.id !== this.id) {
             this.drawRaw(data)
           }
           break;
         case "clear":
-          this.context.clearRect(0 ,0 ,window.innerWidth, window.innerHeight)
+          this.context.clearRect(0, 0, window.innerWidth, window.innerHeight)
         default:
           break;
       }
@@ -59,18 +59,18 @@ Alpine.data("whiteboard", (subject) => ({
         from: from,
         to: to,
         thickness: this.thickness,
-        color: this.color 
+        color: this.color
       }
 
       this.drawRaw(msg)
-      this.nats.publish(subject, this.jc.encode(msg))
+      this.nats.publish(subject, JSON.stringify(msg))
 
       this.last = to
     }, 30)()
   },
 
   getPoint(e) {
-    if(!e.offsetX || !e.offsetY) {
+    if (!e.offsetX || !e.offsetY) {
       const rect = e.target.getBoundingClientRect()
       e.offsetX = (e.touches[0].clientX - window.pageXOffset - rect.left)
       e.offsetY = (e.touches[0].clientY - window.pageYOffset - rect.top)
@@ -82,10 +82,10 @@ Alpine.data("whiteboard", (subject) => ({
     const msg = { id: this.id, type: "clear", }
     const h = headers()
     h.set("Nats-Rollup", "sub")
-    this.nats.publish(subject, this.jc.encode(msg), { headers: h })
+    this.nats.publish(subject, JSON.stringify(msg), { headers: h })
   },
 
-  drawRaw({from, to, thickness, color}) {
+  drawRaw({ from, to, thickness, color }) {
     const c = this.context
     c.beginPath()
     c.lineWidth = thickness
